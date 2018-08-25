78f6d8dcb70a2517bf4faa2bbdb70bb80d3b6951
MrFranciscoPaz <francisco.paz.c@gmail.com>
2018-08-24 20:27:26 -0500

    Gradiente descenso terminado
    1. se agrega el algoritmo de gradiente descenso con su respectiva prueba
    vs lm.

--- 01_tarea_regresion_lineal.R
+++ 01_tarea_regresion_lineal.R
@@ -6,13 +6,54 @@ library(readr)
 library(dplyr)
 library(knitr)
 
+#Declaramos las funciones que vamos a utilizar:
 
-viviendas<-read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data", header = FALSE )
+descenso <- function(n, z_0, eta, h_deriv){
+  z <- matrix(0,n, length(z_0))
+  z[1, ] <- z_0
+  for(i in 1:(n-1)){
+    z[i+1, ] <- z[i, ] - eta * h_deriv(z[i, ])
+  }
+  z
+}
 
-names(viviendas)<-c("CRIM","ZN","INDUS,","CHAS","NOX","RM","AGE","DIS","RAD","TAX","PTRATIO","B","LSTAT","MEDV")
+grad_calc <- function(x_ent, y_ent){
+  salida_grad <- function(beta){
+    f_beta <- as.matrix(cbind(1, x_ent)) %*% beta
+    e <- y_ent - f_beta
+    grad_out <- -as.numeric(t(cbind(1, x_ent)) %*% e)
+    names(grad_out) <- c('Intercept', colnames(x_ent))
+    grad_out
+  }
+  salida_grad
+}
+
+#Para pruebas 
+#iteraciones <- descenso(500000, c(0,0,0,0,0,0,0,0,0,0,0,0,0,0), 0.00005, grad_viv)
+#grad_viv <- grad_calc(viv_entrena_norm[, 1:13, drop = FALSE], viv_entrena_norm[,14])
+
+#Checar la función del error.
+rss_calc <- function(datos){
+  # esta función recibe los datos (x,y) y devuelve
+  # una función f(betas) que calcula rss
+  y <- datos$lpsa
+  x <- datos$lcavol
+  fun_out <- function(beta){
+    y_hat <- beta[1] + beta[2]*x
+    e <- (y - y_hat)
+    rss <- sum(e^2)
+    0.5*rss
+  }
+  fun_out
+}
+
+#Descargamos la información 
+viviendas<-read_table2("https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data", col_names =  FALSE )
+
+names(viviendas)<-c("CRIM","ZN","INDUS","CHAS","NOX","RM","AGE","DIS","RAD","TAX","PTRATIO","B","LSTAT","MEDV")
 names(viviendas)
 #Separamos la muestra
-set.seed(1993)
+set.seed(1923430484)
 
 n <- 400
 N <- nrow(viviendas)
@@ -31,6 +72,51 @@ describe(viv_entrena)
 
 #Normalizamos las variables:
 viv_entrena_norm<-viv_entrena
+
+#Vamos a dividir el ajuste en dos casos: 
+#1° No hacemos reconocimiento entre variables númericas y categoricas.
+
+
+viv_entrena_norm<-as.data.frame(viv_entrena_norm)
+
+
+for (i in 1:13){
+    prom<-mean(viv_entrena_norm[,i] )
+    #print (prom)
+    ed<-sd(viv_entrena_norm[,i])
+    #print(ed)
+    viv_entrena_norm[,i]<-(viv_entrena_norm[,i]-prom)/ed
+}
+describe(viv_entrena_norm)
+
+
+grad_viv <- grad_calc(viv_entrena_norm[, 1:13, drop = FALSE], viv_entrena_norm$MEDV)
+
+grad_viv(c(0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+
+rep(0,14)
+iteraciones <- descenso(500, c(rep(10,14)), 0.0007, grad_viv)
+iteraciones[500,]
+
+#Verificamos que el gradiente convergio, es decir, este se acerca a 0
+grad_viv(iteraciones[500,])
+
+#Ajustamos el modelo con la funcion lm
+model_test<-lm(MEDV ~ CRIM + ZN + INDUS + CHAS + NOX + RM + AGE + DIS + RAD + TAX + PTRATIO + B + LSTAT,data = viv_entrena_norm)
+summary(model_test)
+
+#Podemos observar que la $\beta$ son iguales, por lo que el modelo es correcto.
+
+
+rss_viv <- rss_calc(viv_entrena_norm)
+apply(iteraciones, 1, rss_viv)
+
+
+
+
+
+
+####2° No hacemos reconocimiento entre variables númericas y categoricas.#####
 
 for (i in 1:14){
   if (i != 4)
