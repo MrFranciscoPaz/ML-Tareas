52c5f1bd1a0893e4545074377f68db9e1fc79f05
MrFranciscoPaz <francisco.paz.c@gmail.com>
2018-08-24 22:21:05 -0500

    Agregamos las funciones para errores
    ya se tiene error de entrenamiento y error de prueba|

--- 01_tarea_regresion_lineal.R
+++ 01_tarea_regresion_lineal.R
@@ -4,10 +4,12 @@ library(samplingVarEst)
 library(psych)
 library(readr)
 library(dplyr)
+library(pander)
 library(knitr)
 
 #Declaramos las funciones que vamos a utilizar:
 
+#funcion para el descenso
 descenso <- function(n, z_0, eta, h_deriv){
   z <- matrix(0,n, length(z_0))
   z[1, ] <- z_0
@@ -17,6 +19,7 @@ descenso <- function(n, z_0, eta, h_deriv){
   z
 }
 
+#Funcion que calcula la derivada:
 grad_calc <- function(x_ent, y_ent){
   salida_grad <- function(beta){
     f_beta <- as.matrix(cbind(1, x_ent)) %*% beta
@@ -28,23 +31,30 @@ grad_calc <- function(x_ent, y_ent){
   salida_grad
 }
 
-#Para pruebas 
-#iteraciones <- descenso(500000, c(0,0,0,0,0,0,0,0,0,0,0,0,0,0), 0.00005, grad_viv)
-#grad_viv <- grad_calc(viv_entrena_norm[, 1:13, drop = FALSE], viv_entrena_norm[,14])
-
-#Checar la función del error.
-rss_calc <- function(datos){
+#Funcion para calcular el error:
+rss_calc <- function(x,y){
   # esta función recibe los datos (x,y) y devuelve
   # una función f(betas) que calcula rss
-  y <- datos$lpsa
-  x <- datos$lcavol
   fun_out <- function(beta){
-    y_hat <- beta[1] + beta[2]*x
-    e <- (y - y_hat)
+    y_hat <- as.matrix(cbind(1,x))%*%beta
+    e <- y - y_hat
     rss <- sum(e^2)
     0.5*rss
   }
   fun_out
+}
+
+#Funcion para calcular el error
+error_f <- function(preds,y){
+    round(sqrt(mean((preds - y) ^ 2)),2)
+  }
+
+#Funcion para predicciones
+pred<-function(x){
+  function(betas){
+    f_beta<-as.matrix(cbind(1,x))%*%betas
+    return(f_beta)
+  }
 }
 
 #Descargamos la información 
@@ -53,8 +63,8 @@ viviendas<-read_table2("https://archive.ics.uci.edu/ml/machine-learning-database
 names(viviendas)<-c("CRIM","ZN","INDUS","CHAS","NOX","RM","AGE","DIS","RAD","TAX","PTRATIO","B","LSTAT","MEDV")
 names(viviendas)
 #Separamos la muestra
-set.seed(1923430484)
-
+set.seed(1993)
+#Para usar muestra aleatoria:
 n <- 400
 N <- nrow(viviendas)
 Muestra <- srswor1(n,N)
@@ -68,17 +78,14 @@ viv_prueba<-viviendas[prueba,]
 #Comenzamos co las medidas de tendencia central 
 
 summary(viv_entrena)
-describe(viv_entrena)
+est_des<-describe(viv_entrena)
+est_des
 
 #Normalizamos las variables:
 viv_entrena_norm<-viv_entrena
 
-#Vamos a dividir el ajuste en dos casos: 
-#1° No hacemos reconocimiento entre variables númericas y categoricas.
-
-
+#No hacemos reconocimiento entre variables númericas y categoricas.
 viv_entrena_norm<-as.data.frame(viv_entrena_norm)
-
 
 for (i in 1:13){
     prom<-mean(viv_entrena_norm[,i] )
@@ -87,14 +94,12 @@ for (i in 1:13){
     #print(ed)
     viv_entrena_norm[,i]<-(viv_entrena_norm[,i]-prom)/ed
 }
-describe(viv_entrena_norm)
+
 
 
 grad_viv <- grad_calc(viv_entrena_norm[, 1:13, drop = FALSE], viv_entrena_norm$MEDV)
+grad_viv(c(rep(0,14)))
 
-grad_viv(c(0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-
-rep(0,14)
 iteraciones <- descenso(500, c(rep(10,14)), 0.0007, grad_viv)
 iteraciones[500,]
 
@@ -106,23 +111,36 @@ model_test<-lm(MEDV ~ CRIM + ZN + INDUS + CHAS + NOX + RM + AGE + DIS + RAD + TA
 summary(model_test)
 
 #Podemos observar que la $\beta$ son iguales, por lo que el modelo es correcto.
+#Procedemos a evaluar el error de entrenamiento:
 
-
-rss_viv <- rss_calc(viv_entrena_norm)
+#Verificamos que si disminuye.
+rss_viv<- rss_calc(viv_entrena_norm[, 1:13, drop = FALSE], viv_entrena_norm$MEDV)
 apply(iteraciones, 1, rss_viv)
 
 
+#Finalmente, calculamos error de entrenamiento y error de prueba:
+betas<-iteraciones[500,]
+names(betas)<-c("Intercep",names(viv_entrena[,1:13]))
 
+#Error de entrenamiento:
+pred_entrena<-pred(viv_entrena_norm[,1:13])
+e_entrena<-error_f(pred_entrena(betas),viv_entrena_norm$MEDV)
 
+#aplicamamos la normalizacion a los datos:
+viv_prueba_norm<-viv_prueba
+viv_prueba_norm<-as.data.frame(viv_prueba_norm)
 
-
-####2° No hacemos reconocimiento entre variables númericas y categoricas.#####
-
-for (i in 1:14){
-  if (i != 4)
-  viv_entrena_norm[,i]<-(viv_entrena_norm[,i]-mean(viv_entrena_norm[,i])/sd(viv_entrena_norm[,i]))
+for (i in 1:13){
+  viv_prueba_norm[,i]<-(viv_prueba_norm[,i]-est_des$mean[i])/(est_des$sd[i])
 }
 
 
+# Observamos como nos va con las predicciones
+res_prueba<-pred(viv_prueba_norm[,1:13])
+e_prueba<-error_f(res_prueba(betas),viv_prueba$MEDV)
 
-
+#Observamos que los errorees encontrados son
+e_entrena
+#4.78
+e_prueba
+#4.38
